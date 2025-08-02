3fae45b90c60a6f664a409d7bf095f8f73ab990d
Gopmyc <hoyaux.gaspard@gmail.com>
2025-08-02 18:44:23 +0200

    feat (parser) : ✂️ add static utility function trim_trailing

--- srcs/parser.c
+++ srcs/parser.c
@@ -1,2 +1,13 @@
 #include "docs_builder.h"
 
+//
+// ┌────────────────────────┐
+// │ DOCS GENERATION PARSER │
+// └────────────────────────┘
+//
+static void trim_trailing(char* str)
+{
+	size_t len = strlen(str);
+	while (len && (str[len - 1] == '\n' || str[len - 1] == '\r' || str[len - 1] == ' ' || str[len - 1] == '\t'))
+		str[--len] = 0;
+}
