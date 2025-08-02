67128023604c2cdf1a7e8c4065717a5a5a8438e9
Gopmyc <hoyaux.gaspard@gmail.com>
2025-08-02 18:38:43 +0200

    feat (utils) : 🧩 add endsWithHtml utility function

--- srcs/utils.c
+++ srcs/utils.c
@@ -10,3 +10,13 @@ void	writeIndent(FILE *fOut, int iDepth)
 	for (int i = 0; i < iDepth * INDENT_WIDTH; i++)
 		fputc('\t', fOut);
 }
+
+int	endsWithHtml(const char *sName)
+{
+	size_t iLen = strlen(sName);
+	size_t iExtLen = strlen(EXTENSION);
+
+	return iLen >= iExtLen &&
+		strcmp(sName + iLen - iExtLen, EXTENSION) == 0 &&
+		strcmp(sName, EXCLUDE_FILE) != 0;
+}
