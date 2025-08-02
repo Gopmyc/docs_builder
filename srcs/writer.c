7770fca4f58678a0283e9ddd3b7e103637677c6b
Gopmyc <hoyaux.gaspard@gmail.com>
2025-08-02 18:47:16 +0200

    feat (writer) : ✍️ add write_docblock_html function for generating HTML from docblocks

--- srcs/writer.c
+++ srcs/writer.c
@@ -1,2 +1,46 @@
 #include "docs_builder.h"
 
+//
+// ┌────────────────────────┐
+// │ DOCS GENERATION WRITER │
+// └────────────────────────┘
+//
+void write_docblock_html(FILE* fOut, DocBlock* doc)
+{
+	fprintf(fOut,
+		"<section id=\"%s\">\n"
+		"	<h2>%s</h2>\n"
+		"	<p>\n"
+		"		<li><strong>@desc :</strong></li>\n"
+		"		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; %s\n"
+		"	</p>\n"
+		"	<ul class=\"meta\">\n",
+		doc->name[0] ? doc->name : "unnamed",
+		doc->name[0] ? doc->name : "Unnamed",
+		doc->desc[0] ? doc->desc : "No description."
+	);
+
+	if (doc->params) {
+		fprintf(fOut, "		<li><strong>@param :</strong></li>\n");
+		for (Param* p = doc->params; p; p = p->next)
+			fprintf(fOut, "		<li>&nbsp;&nbsp;&nbsp;&nbsp;<b>{%s}</b> : %s</li>\n", p->type, p->desc);
+	}
+
+	if (doc->returns) {
+		fprintf(fOut, "		<li><strong>@return :</strong></li>\n");
+		for (Return* r = doc->returns; r; r = r->next)
+			fprintf(fOut, "		<li>&nbsp;&nbsp;&nbsp;&nbsp;<b>{%s}</b> : %s</li>\n", r->type, r->desc);
+	}
+
+	fprintf(fOut,
+		"	</ul>\n"
+		"	<div class=\"code-block\">\n"
+		"		<button class=\"copy-btn\">📋</button>\n"
+		"		<pre><code>\n"
+		"		-- Some Code\n"
+		"		</code></pre>\n"
+		"	</div>\n"
+		"</section>\n"
+	);
+}
+
