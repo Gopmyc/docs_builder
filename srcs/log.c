456df3b9802c35bf540718b898ec2adda77cde45
Gopmyc <hoyaux.gaspard@gmail.com>
2025-08-02 18:42:06 +0200

    feat (log) : 🎨 add setConsoleColor function for terminal color output

--- srcs/log.c
+++ srcs/log.c
@@ -1,1 +1,12 @@
 #include "docs_builder.h"
+
+//
+// ┌────────────┐
+// │ LOG SYSTEM │
+// └────────────┘
+//
+void	setConsoleColor(WORD wColor)
+{
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, wColor);
+}
