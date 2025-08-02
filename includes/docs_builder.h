b3c90543d3d58148fa53879b16bf6683548fa940
Gopmyc <hoyaux.gaspard@gmail.com>
2025-08-02 18:33:46 +0200

    chore (docs-builder) : 📚 include required standard and Windows libraries in header

--- includes/docs_builder.h
+++ includes/docs_builder.h
@@ -7,3 +7,17 @@
 //
 #define _CRT_SECURE_NO_WARNINGS
 #define WIN32_LEAN_AND_MEAN
+
+//
+// ┌───────────┐
+// │ LIBRARIES │
+// └───────────┘
+//
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <io.h>
+#include <direct.h>
+#include <sys/stat.h>
+
