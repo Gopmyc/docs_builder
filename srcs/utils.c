8b8ff37ef8da1a32fbfbb65c8114c3df63c60088
Gopmyc <hoyaux.gaspard@gmail.com>
2025-08-02 18:38:27 +0200

    feat (utils) : ✍️ add writeIndent utility function

--- srcs/utils.c
+++ srcs/utils.c
@@ -1,2 +1,12 @@
 #include "docs_builder.h"
 
+//
+// ┌───────────────┐
+// │ UTILITY FUNCS │
+// └───────────────┘
+//
+void	writeIndent(FILE *fOut, int iDepth)
+{
+	for (int i = 0; i < iDepth * INDENT_WIDTH; i++)
+		fputc('\t', fOut);
+}
