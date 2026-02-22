80ff8cd958a3f62507c4ebe61f17576e8b09b73a
Gopmyc <hoyaux.gaspard@gmail.com>
2026-02-23 00:42:02 +0100

    chore(tests/lua/foo_folder/foo_file.lua): ⚙️ - add other files (foo_file.lua)

---
+++ tests/lua/foo_folder/foo_file.lua
@@ -0,0 +1,7 @@
+--[[
+    @name : fOoo
+    @desc : Test function for documentation generation
+    @param {boolean} : A boolean parameter to test the function
+    @return {boolean} : Returns the same boolean value passed as a parameter
+]]
+local function fOoo(bBool) return bBool end
