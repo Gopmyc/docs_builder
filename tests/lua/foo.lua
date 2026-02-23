b7438933790402be0772b0552ece5111b1a87cdf
Gopmyc <hoyaux.gaspard@gmail.com>
2026-02-23 15:44:10 +0100

    chore(tests/lua/foo.lua): 🗑️ - remove other files (foo.lua)

--- tests/lua/foo.lua
+++
@@ -1,7 +0,0 @@
---[[
-    @name : foo
-    @desc : Test function for documentation generation
-    @param {boolean} : A boolean parameter to test the function
-    @return {boolean} : Returns the same boolean value passed as a parameter
-]]
-local function foo(bBool) return bBool end
